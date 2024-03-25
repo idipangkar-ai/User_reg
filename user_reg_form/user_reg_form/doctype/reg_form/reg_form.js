f0e3e65fbebae7013c2c21e40a0cfc08c4a06ac4
Dipangkar <idipangkar02@gmail.com>
2024-03-25 15:13:08 +0530

    added user

---
+++ user_reg_form/user_reg_form/doctype/reg_form/reg_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Dipangkar H and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Reg Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ user_reg_form/user_reg_form/doctype/reg_form/test_reg_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Dipangkar H and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRegForm(FrappeTestCase):
+	pass
